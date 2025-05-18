10fc54c15a6bb6443c16acc7f35532925b50b99a
DHDuong2903 <dohuyduong2903@gmail.com>
2025-05-18 15:34:36 +0700

    mern_auth_completed

---
+++ backend/mailtrap/emails.js
@@ -0,0 +1,72 @@
+import {
+  PASSWORD_RESET_REQUEST_TEMPLATE,
+  PASSWORD_RESET_SUCCESS_TEMPLATE,
+  VERIFICATION_EMAIL_TEMPLATE,
+} from "./emailTemplate.js";
+import { mailtrapClient, sender } from "./mailtrap.config.js";
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Verify Your Email",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
+      category: "Email Verification",
+    });
+  } catch (error) {
+    throw new Error(`Error sending verification: ${error}`);
+  }
+};
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "3af70053-7ad8-4ff8-a7e8-b39926c8d3ec",
+      template_variables: {
+        company_info_name: "Auth Comany",
+        name: name,
+      },
+    });
+  } catch (error) {
+    throw new Error(`Error sending welcome email: ${error}`);
+  }
+};
+
+export const sendPasswordResetEmail = async (email, resetURL) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Reset your password",
+      html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+      category: "Password Reset",
+    });
+  } catch (error) {
+    throw new Error(`Error sending password reset email: ${error}`);
+  }
+};
+
+export const sendResetSuccessEmail = async (email) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Password reset successful",
+      html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+      category: "Password Reset",
+    });
+  } catch (error) {
+    throw new Error(`Error sending password reset success email: ${error}`);
+  }
+};
